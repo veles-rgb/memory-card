59cf275b7bda44fdf58d93a00eebc7edb6361b50
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-07-31 14:43:36 -0400

    Create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import '../styles/Header.css';
+
+function Header() {
+  return (
+    <header>
+      <h1>Knockout Recall</h1>
+      <p>
+        Click on each UFC fighter only once to rack up points — but choose
+        carefully. One wrong move, and it’s game over!
+      </p>
+    </header>
+  );
+}
+
+export default Header;
