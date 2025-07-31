9e55d11c7862ce2f2be9890a6d68c31c2eedf2a3
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-07-30 21:29:46 -0400

    Setup App states / effects & fetchData API call

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,34 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [clickedCards, setClickedCards] = useState([]);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const [hasLost, setHasLost] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsLoading(true);
+      try {
+        const res = await fetch('https://api.octagon-api.com/fighters');
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        const data = await res.json();
+        const fighters = Object.values(data).slice(0, 12);
+        setCards(fighters);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return <></>;
 }
 
